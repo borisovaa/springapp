a58cf5189a0883fae5f7ec065573a79ede8839f8
borisovaa <borisov_aa@bk.ru>
2023-04-05 13:01:24 +0300

    Second Commit

--- src/main/java/ru/home/springcourse/classicalMusic.java
+++ src/main/java/ru/home/springcourse/classicalMusic.java
@@ -1,6 +1,9 @@
 package ru.home.springcourse;
 
 public class classicalMusic implements Music{
+
+    // ПРОВЕРКА РАБОТЫ ГИТ
+
     @Override
     public String getSong() {
         return "Венгерская рапсодия";
